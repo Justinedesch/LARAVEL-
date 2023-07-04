f30ea82436cd7d70e2012ab420e804e5b44ebb3c
Justine DESCHAMPS <justine.deschamps@Digital-Grenoble.local>
2023-07-04 13:28:59 +0200

    etape 2 - mise en place url dans fichier de route

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'homepage';
+});
+
+Route::get('/product', function () {
+    return 'liste des produits';
+});
+
+Route::get('/product/id', function () {
+    return 'Fiche du produit {id}';
+});
+
+Route::get('/cart', function () {
+    return 'Panier';
 });
